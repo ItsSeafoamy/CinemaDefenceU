24c712de13a125cad50fa4261cbfd7cebef4c334
KhorneDisciple <amplifier.prince@hotmail.com>
2015-03-17 18:42:16 +0000

    Update IceCreamGun.cs

--- Assets/Scripts/Towers/IceCreamGun.cs
+++ Assets/Scripts/Towers/IceCreamGun.cs
@@ -1,5 +1,5 @@
 ﻿/*
-*	Copyright (C) 2015 Dylan McCormack
+*	Copyright (C) 2015 Dylan McCormack, Alexander Prince
 *	
 *	This program is free software; you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
